9ca18d6d53af930e3da765d8dee8faf25452bad6
Jonas Spieler <jonas@spieler.io>
2021-09-25 14:40:50 +0200

    Add api

---
+++ api/fast_api.py
@@ -0,0 +1,29 @@
+from typing import Optional
+from page_rank import PageRank
+
+from fastapi import FastAPI
+
+doc_path = f"../data/data_from_2020/*json"
+
+app = FastAPI()
+
+index = PageRank(doc_path)
+
+@app.get("/pairs")
+def read_pairs():
+    return {"Hello": "World"}
+
+@app.get("/terms/{n}")
+def read_terms(n: int):
+    term_list = index.get_terms(n)
+
+    return {}
+
+@app.get("/search/{query}")
+def read_search(query: str):
+    res = index.search(query)
+
+    if len(res) > 0:
+        return {"query": res[0]}
+    else:
+        return {}
---
+++ api/page_rank.py
@@ -0,0 +1,71 @@
+import sklearn
+import os
+import glob2
+import pandas as pd
+import numpy as np
+import json
+import functools
+from prompt_toolkit import prompt
+from inverted_index.prompt import SearchCompleter
+from inverted_index.inverted_index import InvertedIndex, LevenshteinRankingInvertedIndex
+
+class PageRank:
+  def __init__(self, doc_path):
+    self.doc_path = doc_path
+    self.docs_json=[]
+    self.docs=[]
+    self.ii = None
+
+    self.read_docs()
+    self.create_index()
+    
+    self.n_docs = len(self.docs)
+
+  def read_docs(self):
+    for doc in sorted(glob2.glob(self.doc_path)):
+      with open(doc) as f:
+        json_doc = json.load(f)
+        if len(json_doc["paragraphs"]) > 0:
+          self.docs_json.append(json_doc)
+          self.docs.append(functools.reduce((lambda x, y : x + " " + y), json_doc["paragraphs"]))
+    
+    self.n_docs = len(self.docs)
+
+  def create_index(self):
+    self.ii = InvertedIndex()
+    self.ii.index(self.docs)
+
+  def df(self, term):
+    if term in self.ii.posting_list:
+      return len(self.ii.posting_list[term])
+    else:
+      return 0
+
+  # def tf(self, term, docid):
+
+
+
+  def search(self, query):
+    return self.ii.search(query)
+
+  def get_terms(self, n):
+
+    return 0
+
+
+def main():
+  doc_path = f"../data/data_from_2020/*json"
+  query = "Zürich"
+  pr = PageRank(doc_path)
+
+  for r in pr.ii.search(query):
+    print(r[:9])
+
+
+
+  # for i in range(5):
+  #   print(pr.docs[i]["paragraphs"])
+
+
+if __name__ == "__main__":
+  main()
